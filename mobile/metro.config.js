3f145560d5e474ead98940a33d4b78f2137b0b8c
x0bd <synthmancer@gmail.com>
2026-02-20 09:53:06 +0200

    add deps

---
+++ mobile/metro.config.js
@@ -0,0 +1,7 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativewind } = require('nativewind/metro');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativewind(config, { input: './global.css' });
+
---
+++ mobile/types/nutrition.ts
@@ -0,0 +1,10 @@
+export interface NutritionAnalysis {
+  meal_name: string;
+  calories: number;
+  protein: number;
+  carbs: number;
+  fat: number;
+  health_score: number; // 1-100
+  igo_tip: string;
+}
+
